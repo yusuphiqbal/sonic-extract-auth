2a0502fdfe8502e3cc8dd2d6aeae1d174b88e223
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-09-27 09:46:30 +0300

    Created basic express server

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import config from 'config';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
+mongoose.set('strictQuery', true);
+mongoose.connect(config.get('mongoUri')).catch(() => process.exit(1));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan(config.get('morgan.format')));
+
+const port = config.get('port');
+
+app.listen(port, () =>
+  console.log(`🚀 Auth service listening on port ${port}`),
+);
