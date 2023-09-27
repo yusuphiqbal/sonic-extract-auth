685dfedae21a6c890025c70bedd2f946f396337c
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-09-27 09:48:47 +0300

    Registered not-found middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import helmet from 'helmet';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 
+import notFound from './middleware/not-found';
+
 mongoose.set('strictQuery', true);
 mongoose.connect(config.get('mongoUri')).catch(() => process.exit(1));
 
@@ -14,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan(config.get('morgan.format')));
+
+app.use(notFound);
 
 const port = config.get('port');
 
